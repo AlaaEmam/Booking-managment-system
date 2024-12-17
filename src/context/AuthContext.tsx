05044919eb30aa6ea68b53a066615bf236743b9f
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2024-12-17 11:26:49 +0200

    push in my branch

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -20,33 +20,12 @@ interface AuthContextProviderProps {
 export default function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [loginData, setLoginData] = useState<LoginData | null>(null);
 
-  // const saveLoginData = () => {
-  //   const token = localStorage.getItem("token");
-  //   if (token) {
-  //     const decodedToken = jwtDecode<LoginData>(token); // تحديد النوع هنا
-  //     setLoginData(decodedToken);
-  //     console.log(decodedToken)
-  //   }
-  // };
-
   const saveLoginData = () => {
     const token = localStorage.getItem("token");
     if (token) {
-      // Check if the token is in the correct format
-      const parts = token.split('.');
-      if (parts.length === 3) {
-        try {
-          const decodedToken = jwtDecode<LoginData>(token); // Specify the type here
-          setLoginData(decodedToken);
-          console.log(decodedToken);
-        } catch (error) {
-          console.error("Failed to decode token:", error);
-        }
-      } else {
-        console.error("Invalid token format:", token);
-        // Handle invalid token format case, e.g., by clearing the token
-        localStorage.removeItem("token");
-      }
+      const decodedToken = jwtDecode<LoginData>(token); // تحديد النوع هنا
+      setLoginData(decodedToken);
+      console.log(decodedToken)
     }
   };
 
