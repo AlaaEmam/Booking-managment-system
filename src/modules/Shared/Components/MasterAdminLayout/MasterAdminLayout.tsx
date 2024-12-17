31b5f3db870999192b34f21bc9600c1fb633512f
Anas <anasabdo704@gmail.com>
2024-12-17 22:10:51 +0400

    sidebar , navbarAdmin

--- src/modules/Shared/Components/MasterAdminLayout/MasterAdminLayout.tsx
+++ src/modules/Shared/Components/MasterAdminLayout/MasterAdminLayout.tsx
@@ -1,13 +1,33 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "../../../Admin/Components/Shared/Components/SideBar/SideBar";
 import NavbarAdmin from "../../../Admin/Components/Shared/Components/NavbarAdmin/NavbarAdmin";
+import { Box } from "@mui/material";
 export default function MasterAdminLayout() {
   return (
     <div>
-      
-      <NavbarAdmin/>
-      <SideBar/>
-      <Outlet />
+      <Box sx={{ display: "flex", height: "100vh" }}>
+        <Box sx={{ bgcolor: "primary.main", color: "#fff" }}>
+          <SideBar />
+        </Box>
+
+        <Box
+          sx={{
+            width: "100%",
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            p: 2,
+          }}
+        >
+          <Box sx={{}}>
+            <NavbarAdmin />
+          </Box>
+
+          <Box sx={{ flexGrow: 1, overflow: "auto" }}>
+            <Outlet />
+          </Box>
+        </Box>
+      </Box>
     </div>
   );
 }
