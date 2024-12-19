c7020cc47c8488d56a6f8db6b6f1f6cf4e98f5b7
siegfried2020 <domydodo2015@gmail.com>
2024-12-19 21:50:21 +0200

    list-booking-2

--- src/modules/Admin/Components/BookingList/BookingList.tsx
+++ src/modules/Admin/Components/BookingList/BookingList.tsx
@@ -1,9 +1,6 @@
-import { Box,  Modal,  styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableFooter, TableHead, TablePagination, TableRow, Typography } from '@mui/material'
+import { Box,  Modal,  Stack,  styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { ADMINBOOKING, axiosInstance } from '../../../../constants/URLS'
-import { AlignHorizontalRight, BorderAllRounded, Padding } from '@mui/icons-material';
-import TablePaginationActions from '@mui/material/TablePagination/TablePaginationActions';
-import { grey } from '@mui/material/colors';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import ClearIcon from '@mui/icons-material/Clear';
 interface BookingListProps{
@@ -31,7 +28,7 @@ export default function BookingList() {
     try{
       let response= await axiosInstance.get(ADMINBOOKING.getAllBooking);
       console.log(response.data.data.booking);
-      setBookingList(response?.data?.data.booking)
+      setBookingList(response?.data?.data?.booking)
     }catch(error){
       console.log(error)
     }
@@ -87,30 +84,64 @@ export default function BookingList() {
   
   return (
     
-    <div>
+    <Box>
       <Modal
         open={showView}
-        aria-labelledby="parent-modal-title"
-        aria-describedby="parent-modal-description"
+        onClose={handleCloseView}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
-        { selectedBooking &&(
+        
+        <Box sx={{ ...style, width: 500 }}>
+            <Stack alignItems="center" justifyContent={'space-between'} direction="row" gap={2}>
 
-          
-          <Box sx={{...style, width: 400 }}>
-            <ClearIcon color='warning' onClick={handleCloseView}/>
-            <Typography color='primary' variant='h5'>{selectedBooking?.user.userName}'s booking room </Typography>
-            <Typography variant='subtitle1'>
-              <span style={{color:"rgb(52, 39, 234)"}}>Room Number:</span> {selectedBooking?.room?.roomNumber}</Typography>
-            <Typography variant='subtitle1'>
-              <span style={{color:"rgb(52, 39, 234)"}}>Total Price:</span> {selectedBooking?.totalPrice}</Typography>
-            <Typography variant='subtitle1'>
-              <span style={{color:"rgb(52, 39, 234)"}}>Start Date:</span> {selectedBooking?.startDate}</Typography>
-            <Typography variant='subtitle1'>
-              <span style={{color:"rgb(52, 39, 234)"}}>End Date:</span> {selectedBooking?.endDate}</Typography>
+              <Typography color='success' id="modal-modal-title" variant="h5" component="h2">
+                {selectedBooking?.user.userName}'s booking
+              </Typography>
+              <ClearIcon color='warning' onClick={handleCloseView}/>
+            </Stack>
             
-          </Box>
-          )
-        }
+            <Stack alignItems="center" direction="row" gap={2}>
+
+              <Typography color='primary' variant="h5" component="h2">
+                Room Number:
+              </Typography>
+              <Typography color='info' variant="h6">
+                {selectedBooking?.room.roomNumber}
+              </Typography>
+              
+            </Stack>
+            
+            <Stack alignItems="center" direction="row" gap={2}>
+
+              <Typography color='primary' variant="h5" component="h2">
+                Total Price:
+              </Typography>
+              <Typography color='info' variant="h6">
+                {selectedBooking?.totalPrice}
+              </Typography>
+            </Stack>
+
+            <Stack alignItems="center" direction="row" gap={2}>
+
+              <Typography color='primary'  variant="h5" component="h2">
+                Start Date:
+              </Typography>
+              <Typography color='info' variant="h6">
+                {selectedBooking?.startDate}
+              </Typography>
+            </Stack>
+
+            <Stack alignItems="center" direction="row" gap={2}>
+
+              <Typography color='primary' variant="h5" component="h2">
+                End Date:
+              </Typography>
+              <Typography color='info' variant="h6">
+                {selectedBooking?.endDate}
+              </Typography>
+            </Stack>
+        </Box>
       </Modal>
       <Box sx={{marginBottom:"5rem"}}>
 
@@ -118,6 +149,38 @@ export default function BookingList() {
         <Typography variant='subtitle1'>You can check all details</Typography>
       </Box>
 
+      {bookingList.length>0 ?
+        <TableContainer>
+          <Table>
+            <TableHead >
+              <TableRow sx={{"& th":{backgroundColor:"lightgray"}}}>
+                <StyledTableCell>room number</StyledTableCell>
+                <StyledTableCell>price</StyledTableCell>
+                <StyledTableCell>start date</StyledTableCell>
+                <StyledTableCell>end date</StyledTableCell>
+                <StyledTableCell>User</StyledTableCell>
+                <StyledTableCell>Action</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+
+              {bookingList?.map((booking:BookingListProps)=>
+                <StyledTableRow key={booking._id}>
+                  <StyledTableCell>{booking.room.roomNumber}</StyledTableCell>
+                  <StyledTableCell>{booking?.totalPrice}</StyledTableCell>
+                  <StyledTableCell>{booking?.startDate}</StyledTableCell>
+                  <StyledTableCell>{booking?.endDate}</StyledTableCell>
+                  <StyledTableCell>{booking?.user.userName}</StyledTableCell>
+                  <StyledTableCell>
+                    <VisibilityIcon onClick={() => handleShowView(booking)} sx={{color:"rgb(0, 132, 220)"}}/>
+                  </StyledTableCell>
+                </StyledTableRow>
+              )}
+
+            </TableBody>
+          </Table>
+        </TableContainer>
+      :
       <TableContainer>
         <Table>
           <TableHead >
@@ -131,26 +194,13 @@ export default function BookingList() {
             </TableRow>
           </TableHead>
           <TableBody>
-            
-            {bookingList?.map((booking:BookingListProps)=>
-              <StyledTableRow key={booking._id}>
-                <StyledTableCell>{booking.room.roomNumber}</StyledTableCell>
-                <StyledTableCell>{booking?.totalPrice}</StyledTableCell>
-                <StyledTableCell>{booking?.startDate}</StyledTableCell>
-                <StyledTableCell>{booking?.endDate}</StyledTableCell>
-                <StyledTableCell>{booking?.user.userName}</StyledTableCell>
-                <StyledTableCell>
-                  <VisibilityIcon onClick={() => handleShowView(booking)} sx={{color:"rgb(0, 132, 220)"}}/>
-                </StyledTableCell>
-              </StyledTableRow>
-            )}
-             
-          </TableBody>
 
-          
+            <Typography sx={{textAlign:'center'}} variant='h1'>No Data</Typography>
+
+          </TableBody>
         </Table>
-      </TableContainer>
-    </div>
+      </TableContainer>}
+    </Box>
 
     
   )
