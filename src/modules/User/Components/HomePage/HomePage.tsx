176ce4c3d0bd26fbbfa90e8d3cecda69762270a2
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2024-12-26 15:44:19 +0200

    finish review design

--- src/modules/User/Components/HomePage/HomePage.tsx
+++ src/modules/User/Components/HomePage/HomePage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ExploreSection from './../Explore/ExploreSection';
 import FavoriteRoomSection from './../FavoriteRoomPage/FavoriteRoomSection';
 import DataHomePage from '../DataHomePage/DataHomePage';
-import UsersReview from '../UsersReview/UsersReview';
+import UsersReview from './../UsersReview/UsersReview';
 
 export default function HomePage() {
   return (
