4ff206a78398a0aadd862bccb4168e51674e6ab7
ahmednasrcs <hamadnasrcs@gmail.com>
2024-12-30 01:48:00 +0200

    done

--- src/modules/Admin/Components/Shared/Components/SideBar/SideBar.tsx
+++ src/modules/Admin/Components/Shared/Components/SideBar/SideBar.tsx
@@ -1,17 +1,10 @@
 import { useState } from "react";
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-<<<<<<< HEAD
-import { Link, useNavigate } from "react-router-dom";
-
-export default function SideBar() {
-  let navigate = useNavigate();
-=======
 import { Link } from "react-router-dom";
 import { useAuth } from "../../../../../../context/AuthContext";
 
 export default function SideBar() {
   const { clearLoginData } = useAuth();
->>>>>>> 2d2346bb729f0cea3ec9ed4b08a546d2ca1db394
   const [isCollapse, setIsCollapse] = useState(false);
   const toggleCollpase = () => {
     setIsCollapse(!isCollapse);
@@ -21,11 +14,7 @@ export default function SideBar() {
     <div className="sidebar-container">
       <Sidebar collapsed={isCollapse}>
         <Menu>
-<<<<<<< HEAD
-          <divnp
-=======
           <div
->>>>>>> 2d2346bb729f0cea3ec9ed4b08a546d2ca1db394
             onClick={toggleCollpase}
             style={{
               cursor: "pointer",
@@ -41,52 +30,6 @@ export default function SideBar() {
             />{" "}
           </div>
           <MenuItem
-<<<<<<< HEAD
-  icon={<i className="bi bi-house-door"></i>}
-  component={<Link to="/dashboard" />}>
-  Home
-</MenuItem>
-<MenuItem
-  icon={<i className="bi bi-people"></i>}
-  component={<Link to="/dashboard/users-list" />}>
-  Users
-</MenuItem>
-
-<MenuItem
-  icon={<i className="bi bi-columns-gap"></i>}
-  component={<Link to="/dashboard/rooms-list" />}>
-  Rooms
-</MenuItem>
-<MenuItem
-  icon={<i className="bi bi-columns-gap"></i>}
-  component={<Link to="/dashboard/ads-list" />}>
-  Ads
-</MenuItem>
-<MenuItem
-  icon={<i className="bi bi-columns-gap"></i>}
-  component={<Link to="/dashboard/booking-list" />}>
-  Bookings
-</MenuItem>
-<MenuItem
-  icon={<i className="bi bi-columns-gap"></i>}
-  component={<Link to="/dashboard/room-facility" />}>
-  Facilities
-</MenuItem>
-<MenuItem
-  icon={<i className="bi bi-lock"></i>}
-  component={<Link to="/dashboard/change-password" />}>
-  Change Password
-</MenuItem>
-<MenuItem
-  icon={<i className="bi bi-box-arrow-right"></i>}
-  component={<Link to="/" />}
-  onClick={() => {
-    localStorage.removeItem("token");
-  }}>
-  Logout
-</MenuItem>
-
-=======
             icon={<i className="bi bi-house-door"></i>}
             component={<Link to="/dashboard" />}
           >
@@ -138,7 +81,6 @@ export default function SideBar() {
           >
             Logout
           </MenuItem>
->>>>>>> 2d2346bb729f0cea3ec9ed4b08a546d2ca1db394
         </Menu>
       </Sidebar>
     </div>
