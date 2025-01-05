953dd5336d64085694f19a6321bc8a244c7d6ad8
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2025-01-05 18:03:43 +0200

    fix logo path

--- src/modules/Shared/Components/Notfound/Notfound.tsx
+++ src/modules/Shared/Components/Notfound/Notfound.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../../../../assets/logo.svg';
+import Logo from '../../../../assets/Logo.svg';
 import ErrorImage from '../../../../assets/NotFound.svg'; // Path to your 404 image
 import { Link } from 'react-router-dom';
 import { Box, Grid, Typography, Button } from '@mui/material';
